8e9e442446bfde8f546f0bfc0644c84dc2632e28
wbcastilho <welber.castilho@yahoo.com.br>
2022-07-04 15:42:29 -0300

    Ajustes nos componentes product-item e product-list, funcionando com dados estaticos

--- src/app/components/ecommerce/product-list/product-list.component.ts
+++ src/app/components/ecommerce/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: Array<Product> = [
+    {
+      id: 1,
+      name: 'Máquina de Lavar 15Kg Electrolux',
+      price: 1500.00,
+      quantity: 10,
+      category: 'Eletrodomésticos',
+      img: ''
+    },
+    {
+      id: 2,
+      name: 'Televisão LG 45 polegadas',
+      price: 3000.00,
+      quantity: 6,
+      category: 'TV',
+      img: ''
+    },
+    {
+      id: 3,
+      name: 'Geladeira Consul',
+      price: 5000.00,
+      quantity: 6,
+      category: 'Eletrodomésticos',
+      img: ''
+    }
+  ];
 
   constructor() { }
 
