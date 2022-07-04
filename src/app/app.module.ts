1f8420560a0b7702b703e96ea66c47851c438ae7
wbcastilho <welber.castilho@yahoo.com.br>
2022-07-04 13:57:38 -0300

    Instalado e configurado bootstrap e criados componentes core/header, core/footer, ecommerce/product-list, ecommerce/product-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/core/header/header.component';
+import { FooterComponent } from './components/core/footer/footer.component';
+import { ProductListComponent } from './components/ecommerce/product-list/product-list.component';
+import { ProductItemComponent } from './components/ecommerce/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductItemComponent    
   ],
   imports: [
     BrowserModule,
